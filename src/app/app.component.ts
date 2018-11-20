27bbffa1ac807080dad17a45dc674fa78cdebe64
Younes Zaid <zaidyounes94@gmail.com>
2018-11-20 22:44:37 +0100

    added the delete function and updated style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,20 @@ import { Component } from "@angular/core";
 export class AppComponent {
   title = "angular-todo-app";
 
-  todo = "";
+  todo = null;
   todosArray = [];
 
   addTodo = value => {
     this.todosArray.push(value);
+    this.todo = null;
     console.log(this.todosArray);
   };
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todosArray.length; i++) {
+      if (todo == this.todosArray[i]) {
+        this.todosArray.splice(i, 1);
+      }
+    }
+  }
 }
