07a0f20cd546f2fb4183adfa87490386cd5c7ab5
Younes Zaid <zaidyounes94@gmail.com>
2018-11-21 16:58:57 +0100

    added some shit annimation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
 import { Component } from "@angular/core";
+import {
+  trigger,
+  animate,
+  style,
+  transition,
+  keyframes
+} from "@angular/animations";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
+  animations: [
+    trigger("moveInLeft", [
+      transition("void=> *", [
+        style({ transform: "translateX(300px)" }),
+        animate(
+          200,
+          keyframes([
+            style({ transform: "translateX(300px)" }),
+            style({ transform: "translateX(0)" })
+          ])
+        )
+      ]),
+      transition("*=>void", [
+        style({ transform: "translateX(0px)" }),
+        animate(
+          100,
+          keyframes([
+            style({ transform: "translateX(0px)" }),
+            style({ transform: "translateX(300px)" })
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = "angular-todo-app";
@@ -17,11 +48,7 @@ export class AppComponent {
     console.log(this.todosArray);
   };
 
-  deleteItem(todo) {
-    for (let i = 0; i <= this.todosArray.length; i++) {
-      if (todo == this.todosArray[i]) {
-        this.todosArray.splice(i, 1);
-      }
-    }
+  deleteItem(index) {
+    this.todosArray.splice(index, 1);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms"; // to capture the inputs value
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
